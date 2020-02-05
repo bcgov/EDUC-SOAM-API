36bf8951448130551472c18221c035ca659ef073
Mingwei <mingwei.y@gmail.com>
2020-02-05 12:56:19 -0800

    Fix: Add @EnableResourceServer annotation to the controller

--- api/src/main/java/ca/bc/gov/educ/api/soam/controller/SoamController.java
+++ api/src/main/java/ca/bc/gov/educ/api/soam/controller/SoamController.java
@@ -5,6 +5,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import ca.bc.gov.educ.api.soam.endpoint.SoamEndpoint;
 import ca.bc.gov.educ.api.soam.model.entity.ServicesCardEntity;
@@ -18,6 +19,7 @@ import ca.bc.gov.educ.api.soam.service.SoamService;
  */
 
 @RestController
+@EnableResourceServer
 public class SoamController implements SoamEndpoint {
 
     private final SoamService service;

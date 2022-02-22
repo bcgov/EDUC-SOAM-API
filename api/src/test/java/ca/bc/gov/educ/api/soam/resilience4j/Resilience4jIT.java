474a5d18a2597f07518695888360677bfe83446f
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-02-22 15:07:18 -0800

    Setting automatched to a date.

--- api/src/test/java/ca/bc/gov/educ/api/soam/resilience4j/Resilience4jIT.java
+++ api/src/test/java/ca/bc/gov/educ/api/soam/resilience4j/Resilience4jIT.java
@@ -264,7 +264,7 @@ public class Resilience4jIT extends AbstractIntegrationTest {
 	private DigitalIDEntity getDigitalIdentity() {
 		final DigitalIDEntity entity = DigitalIDEntity.builder()
 			.identityTypeCode("BASIC")
-      .autoMatched("N")
+      .autoMatchedDate(null)
 			.identityValue(this.guid)
 			.lastAccessChannelCode("OSPR")
 			.lastAccessDate(LocalDateTime.now().toString())
